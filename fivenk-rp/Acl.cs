4ee7482a2abc98d4fc66d82db26905fc6a6894bc
Emil Maric <emil.maric@hotmail.com>
2017-01-19 22:56:46 -0800

    Pushing half-completed code for dylan to look at for adding commands based on dynamic ACL

---
+++ fivenk-rp/Acl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fivenk_rp
+{
+    public enum Acl
+    {
+        Default,
+        Moderator,
+        Admin
+    }
+}
---
+++ sql/0003.sql
@@ -0,0 +1,2 @@
+-- Add the player's ACL
+ALTER TABLE players ADD Acl Integer NOT NULL DEFAULT 0;
