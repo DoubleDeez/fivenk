418bc30e6910d9ca1d990ca55b11dd0539e7f59f
Dylan Dumesnil <dadumesnil@gmail.com>
2017-01-24 00:10:38 -0800

    Converted rest of speedometer files to spaces

--- resources/fivenk-rp/Client/Speedometer.js
+++ resources/fivenk-rp/Client/Speedometer.js
@@ -2,7 +2,7 @@ var speedometerBrowser = null;
 var wasInVehicle = false;
 
 API.onResourceStart.connect(function() {
-	// In 2560x1440 I want it placed at (450, 1300) so normalize:
+    // In 2560x1440 I want it placed at (450, 1300) so normalize:
     var res = API.getScreenResolution();
     var xPos = 450 * (res.Width / 2560.0);
     var yPos = 1300 * (res.Height / 1440.0);
@@ -14,32 +14,32 @@ API.onResourceStart.connect(function() {
 });
 
 API.onUpdate.connect(function() {
-	var player = API.getLocalPlayer();
-	var isPlayerInVehicle = API.isPlayerInAnyVehicle(player);
+    var player = API.getLocalPlayer();
+    var isPlayerInVehicle = API.isPlayerInAnyVehicle(player);
 
-	if (speedometerBrowser != null) {
-		if (isPlayerInVehicle) {
-			var car = API.getPlayerVehicle(player);
-			var health = API.getVehicleHealth(car);
-			var rpm = API.getVehicleRPM(car);
-			var velocity = API.getEntityVelocity(car);
-			var speed = Math.sqrt(
-				velocity.X * velocity.X +
-				velocity.Y * velocity.Y +
-				velocity.Z * velocity.Z
-				);
+    if (speedometerBrowser != null) {
+        if (isPlayerInVehicle) {
+            var car = API.getPlayerVehicle(player);
+            var health = API.getVehicleHealth(car);
+            var rpm = API.getVehicleRPM(car);
+            var velocity = API.getEntityVelocity(car);
+            var speed = Math.sqrt(
+                velocity.X * velocity.X +
+                velocity.Y * velocity.Y +
+                velocity.Z * velocity.Z
+                );
 
-			// m/s to km/h
-			speedometerBrowser.call("updateSpeed", speed * 3.6);
+            // m/s to km/h
+            speedometerBrowser.call("updateSpeed", speed * 3.6);
 
-			if (!wasInVehicle) {
-				API.setCefBrowserHeadless(speedometerBrowser, false);
-			}
-		} else if (wasInVehicle) {
-			API.setCefBrowserHeadless(speedometerBrowser, true);
-		}
-	}
-	wasInVehicle = isPlayerInVehicle;
+            if (!wasInVehicle) {
+                API.setCefBrowserHeadless(speedometerBrowser, false);
+            }
+        } else if (wasInVehicle) {
+            API.setCefBrowserHeadless(speedometerBrowser, true);
+        }
+    }
+    wasInVehicle = isPlayerInVehicle;
 });
 
 API.onResourceStop.connect(function() {
